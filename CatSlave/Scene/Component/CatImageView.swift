59c1c4f6fc20b3605cdf74b1352368ecad2a23c2
유인호 <yuiop1029@naver.com>
2024-09-01 13:57:22 +0900

    [Feat] Fix Code

--- CatSlave/Scene/Component/CatImageView.swift
+++ CatSlave/Scene/Component/CatImageView.swift
@@ -10,35 +10,41 @@ import Kingfisher
 
 struct CatImageView: View {
 	let item: CatImageDTOModel
+	@State var data: Data?
 	
 	var body: some View {
-		if item.type == .png {
-			KFImage(item.url)
-				.resizable()
-				.placeholder {
-					Color.gray
-						.aspectRatio(item.ratio, contentMode: .fit)
-				}
-				.serialize(as: .JPEG, jpegCompressionQuality: 0.3)
-				.aspectRatio(item.ratio, contentMode: .fit)
-				.clipShape(.rect(cornerRadius: 12))
-				.overlay {
-					RoundedRectangle(cornerRadius: 12)
-						.strokeBorder(.gray.opacity(0.8))
-				}
-			
-		} else {
-			KFAnimatedImage(item.url)
-				.placeholder {
-					Color.gray
-						.aspectRatio(item.ratio, contentMode: .fit)
-				}
-				.aspectRatio(item.ratio, contentMode: .fit)
-				.clipShape(.rect(cornerRadius: 12))
-				.overlay {
-					RoundedRectangle(cornerRadius: 12)
-						.strokeBorder(.gray.opacity(0.8))
-				}
+		Group {
+			if item.type == .png {
+				loadImageView(url: item.url)
+					.onSuccess { data = $0.data() }
+			} else {
+				loadAnimatedImageView(url: item.url)
+					.onSuccess { data = $0.data() }
+			}
 		}
+		.aspectRatio(item.ratio, contentMode: .fit)
+		.clipShape(RoundedRectangle(cornerRadius: 12))
+		.overlay {
+			RoundedRectangle(cornerRadius: 12)
+				.strokeBorder(Color.gray.opacity(0.8))
+		}
+	}
+	
+	private func loadImageView(url: URL?) -> KFImage {
+		KFImage(url)
+			.resizable()
+			.placeholder {
+				Color.gray
+					.aspectRatio(item.ratio, contentMode: .fit)
+			}
+			.serialize(as: .JPEG, jpegCompressionQuality: 0.3)
+	}
+	
+	private func loadAnimatedImageView(url: URL?) -> KFAnimatedImage {
+		KFAnimatedImage(url)
+			.placeholder {
+				Color.gray
+					.aspectRatio(item.ratio, contentMode: .fit)
+			}
 	}
 }
